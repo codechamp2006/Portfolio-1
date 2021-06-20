2be64834dd881e6f64deb3e5a89e277cb0b910d2
codechamp2006 <sagnikraychess@gmail.com>
2021-06-20 18:56:47 +0530

    Connection speed

--- routing.js
+++ routing.js
@@ -1,4 +1,5 @@
 // Credits - Stack Overflow
+// Latency code
 // link -- https://stackoverflow.com/a/21372151
 var arrTimes = [];
 var i = 0; // start
@@ -34,4 +35,66 @@ function testLatency(cb) {
     var avg = sum / arrTimes.length;
     cb(avg);
   }
+}
+
+// Speed code in mbps
+// link -- https://stackoverflow.com/a/5529841
+var imageAddr = "https://ik.imagekit.io/gna0wch172q/icons8-youtube-studio-48_vj3EwgZw0.png"; 
+var downloadSize = 4995374; //bytes
+
+function ShowProgressMessage(msg) {
+    if (console) {
+        if (typeof msg == "string") {
+            console.log(msg);
+        } else {
+            for (var i = 0; i < msg.length; i++) {
+                console.log(msg[i]);
+            }
+        }
+    }
+    
+    var oProgress = document.getElementById("progress");
+    if (oProgress) {
+        var actualHTML = (typeof msg == "string") ? msg : msg.join("<br />");
+        oProgress.innerHTML = actualHTML;
+    }
+}
+
+function InitiateSpeedDetection() {
+    ShowProgressMessage("Loading the image, please wait...");
+    window.setTimeout(MeasureConnectionSpeed, 1);
+};    
+
+if (window.addEventListener) {
+    window.addEventListener('load', InitiateSpeedDetection, false);
+} else if (window.attachEvent) {
+    window.attachEvent('onload', InitiateSpeedDetection);
+}
+
+function MeasureConnectionSpeed() {
+    var startTime, endTime;
+    var download = new Image();
+    download.onload = function () {
+        endTime = (new Date()).getTime();
+        showResults();
+    }
+    
+    download.onerror = function (err, msg) {
+        ShowProgressMessage("Invalid image, or error downloading");
+    }
+    
+    startTime = (new Date()).getTime();
+    var cacheBuster = "?nnn=" + startTime;
+    download.src = imageAddr + cacheBuster;
+    
+    function showResults() {
+        var duration = (endTime - startTime) / 1000;
+        var bitsLoaded = downloadSize * 8;
+        var speedBps = (bitsLoaded / duration).toFixed(2);
+        var speedKbps = (speedBps / 1024).toFixed(2);
+        var speedMbps = (speedKbps / 1024).toFixed(2);
+
+        return document.getElementById("mbpsSpeed").innerHTML = (
+            "Your connection speed is:" + speedMbps + "Mbps"); 
+    }
 }
